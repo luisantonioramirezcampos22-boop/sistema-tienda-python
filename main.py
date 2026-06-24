3feb0665f71befaa434f71e0ee90780a73054563
luisantonioramirezcampos22-boop <luisantonioramirezcampos22@gmail.com>
2026-06-24 14:44:21 -0600

    Actualizar main.py

--- main.py
+++ main.py
@@ -66,10 +66,11 @@ def menu():
                 print("Entrada inválida.")
                 
         elif opcion == "5":
-            if not inv.inventario:
-                print("El catálogo está vacío.")
-            for p in inv.inventario.values():
-    p.mostrar_datos()
+    if not inv.inventario:
+        print("El catálogo está vacío.")
+    else:
+        for p in inv.inventario.values():
+            p.mostrar_datos()
                 
         elif opcion == "6":
             agotados = [p for p in inv.inventario.values() if p.stock == 0]
